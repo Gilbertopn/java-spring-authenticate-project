a87feb3cf850fc023f0ef180449be932387fd412
Gilberto Junior <juninhossj4@gmail.com>
2023-12-17 19:45:32 -0300

    Foi criado o UsuarioMapper para auxiliar o os DTOS, e aplicados no controller

--- authenticate/src/main/java/com/gilberto/authenticate/web/controller/UsuarioController.java
+++ authenticate/src/main/java/com/gilberto/authenticate/web/controller/UsuarioController.java
@@ -2,6 +2,9 @@ package com.gilberto.authenticate.web.controller;
 
 import com.gilberto.authenticate.entity.Usuario;
 import com.gilberto.authenticate.service.UsuarioService;
+import com.gilberto.authenticate.web.dto.UsuarioCreateDto;
+import com.gilberto.authenticate.web.dto.UsuarioResponseDto;
+import com.gilberto.authenticate.web.dto.mapper.UsuarioMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,9 +20,9 @@ public class UsuarioController {
     private final UsuarioService usuarioService;
 
     @PostMapping
-    public ResponseEntity<Usuario> create(@RequestBody Usuario usuario) {
-        Usuario user = usuarioService.salvar(usuario);
-        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    public ResponseEntity<UsuarioResponseDto> create(@RequestBody UsuarioCreateDto createDto) {
+        Usuario user = usuarioService.salvar(UsuarioMapper.toUsuario(createDto));
+        return ResponseEntity.status(HttpStatus.CREATED).body(UsuarioMapper.toDto(user));
     }
 
     @GetMapping("/{id}")
---
+++ authenticate/src/main/java/com/gilberto/authenticate/web/dto/mapper/UsuarioMapper.java
@@ -0,0 +1,29 @@
+package com.gilberto.authenticate.web.dto.mapper;
+
+import com.gilberto.authenticate.entity.Usuario;
+import com.gilberto.authenticate.web.dto.UsuarioCreateDto;
+import com.gilberto.authenticate.web.dto.UsuarioResponseDto;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.PropertyMap;
+
+public class UsuarioMapper {
+
+    public static Usuario toUsuario(UsuarioCreateDto createDto) {
+        return new ModelMapper().map(createDto, Usuario.class);
+    }
+
+    public static UsuarioResponseDto toDto(Usuario usuario) {
+        String role = usuario.getRole().name().substring("Role_".length());
+        PropertyMap<Usuario, UsuarioResponseDto> props =
+                new PropertyMap<Usuario, UsuarioResponseDto>() {
+                    @Override
+                    protected void configure() {
+                        map().setRole(role);
+                    }
+                };
+        ModelMapper mapper = new ModelMapper();
+        mapper.addMappings(props);
+        return mapper.map(usuario, UsuarioResponseDto.class);
+
+    }
+}
